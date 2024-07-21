4d8387a2640cb1dd90ebdbe6425c01a228448a1f
ananyagopal2000 <60483936+ananyagopal2000@users.noreply.github.com>
2024-07-21 14:12:56 +0530

    pushing code before deploying app

---
+++ app/recipe/urls.py
@@ -0,0 +1,25 @@
+"""
+Url mappings for the recipe file
+"""
+
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+from recipe import views
+
+
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
+router.register('Ingredients', views.IngredientViewSet)
+
+
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
